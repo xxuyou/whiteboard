6147c8c49866b0d30c26b20ded3fca59e6d1fd55
guanxuejun@gmail.com <guanxuejun@gmail.com>
2017-06-07 12:47:31 +0800

    baseline

---
+++ src/common/config/demo.js
@@ -0,0 +1,16 @@
+'use strict';
+/**
+ * demo config
+ *
+ */
+export default {
+  app:  'demo_stock',
+  auth: 'XKPPJQYZdrPhWYFZb8mkp0GjEJxqtT4sfCYwxqN8',
+  api:  'http://xxuyou.com/rest',
+  wss:  'http://xxuyou.com',
+  jwt: {
+    options: {
+      algorithm: 'HS256'
+    }
+  }
+}
---
+++ src/common/service/jwt.js
@@ -0,0 +1,79 @@
+'use strict';
+let JWT = require('jsonwebtoken'); // @see https://github.com/auth0/node-jsonwebtoken
+export default class extends think.service.base {
+  /**
+   * init
+   * @return {}         []
+   */
+  init(...args){
+    super.init(...args);
+  }
+
+  /**
+   * generate a JWT token
+   * @see https://github.com/auth0/node-jsonwebtoken
+   * @use
+   * let JWT = new (think.service('jwt'))();
+   * let token = await JWT.sign(payload, priAuth, options).catch((err) => err);
+   * this.write(token);
+   * this.end();
+   *
+   * @param payload
+   * @param secretOrPrivateKey
+   * @param options
+   * @returns {Promise}
+   */
+  async sign(payload, secretOrPrivateKey, options) {
+    return new Promise(function(resolve, reject) {
+      JWT.sign(payload, secretOrPrivateKey, options, function (err, token) {
+        if (err) {
+          reject({
+            err: 20000,
+            msg: err.name + ':' + err.message,
+            data: null
+          });
+        } else {
+          resolve({
+            err: 0,
+            msg: 'ok',
+            data: token
+          });
+        };
+      });
+    });
+  }
+
+  /**
+   * verify a JWT token
+   * @see https://github.com/auth0/node-jsonwebtoken
+   * @use
+   * let JWT = new (think.service('jwt'))();
+   * let verify = await JWT.verify(token, priAuth, options).catch((err) => err);
+   * this.write(verify);
+   * this.end();
+   *
+   * @param token
+   * @param secretOrPublicKey
+   * @param options
+   * @returns {Promise}
+   */
+  async verify(token, secretOrPublicKey, options) {
+    return new Promise(function(resolve, reject) {
+      JWT.verify(token, secretOrPublicKey, options, function (err, decoded) {
+        if (err) {
+          reject({
+            err: 20000,
+            msg: err.name + ':' + err.message,
+            data: null
+          });
+        } else {
+          resolve({
+            err: 0,
+            msg: 'ok',
+            data: decoded
+          });
+        };
+      });
+    });
+  }
+}
